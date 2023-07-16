ec46ca9986d757814a916b347141ecd1996d93fd
tscripca <scripcatudor1@gmail.com>
2023-07-16 22:20:05 +0100

    Issues filling the 2D array slots

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -5,48 +5,25 @@ namespace SlotMachine // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            Random rng = new Random();
             int[,] slotMachine = new int[3, 3];
-            bool playButton = true;
-            while (playButton)
-            {
-                Console.Clear();
-                Random rng = new Random();
-                int randomNumberOne = rng.Next(0, 10);
-                int randomNumberTwo = rng.Next(0, 10);
-                int randomNumberThree = rng.Next(0, 10);
-                int randomNumberFour = rng.Next(0, 10);
-                int randomNumberFive = rng.Next(0, 10);
-                int randomNumberSix = rng.Next(0, 10);
-                int randomNumberSeven = rng.Next(0, 10);
-                int randomNumberEight = rng.Next(0, 10);
-                int randomNumberNine = rng.Next(0, 10);
-                int randomNumberTen = rng.Next(0, 10);
-                //top row
-                slotMachine[0, 0] = randomNumberOne;
-                slotMachine[0, 1] = randomNumberTwo;
-                slotMachine[0, 2] = randomNumberThree;
-                //middle row
-                slotMachine[1, 0] = randomNumberFour;
-                slotMachine[1, 1] = randomNumberFive;
-                slotMachine[1, 2] = randomNumberSix;
-                //bottom row
-                slotMachine[2, 0] = randomNumberSeven;
-                slotMachine[2, 1] = randomNumberEight;
-                slotMachine[2, 2] = randomNumberNine;
-                for (int i = 0; i < slotMachine.GetLength(0); i++)
+
+            for (int collumnIndex = 0; collumnIndex < slotMachine.GetLength(0); collumnIndex++)
+            {                
+                int randomCollumnNumber = rng.Next(0, 10);
+                slotMachine[collumnIndex, 0] = randomCollumnNumber;
+
+                Console.Write(randomCollumnNumber +  " ");
+
+                for (int rowIndex = 0; rowIndex < slotMachine.GetLength(1); rowIndex++)
                 {
-                    for (int j = 0; j < slotMachine.GetLength(1); j++)
-                    {
-                        Console.Write(slotMachine[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                //check the middle row
-                if (randomNumberFour == 7 && randomNumberFour == randomNumberFive && randomNumberFour == randomNumberSix)
-                {
-                    Console.WriteLine("You won the big prize!");
-                    break;
-                }
+                    int randomRowNumber = rng.Next(0, 10);
+                    slotMachine[0, rowIndex] = randomRowNumber;
+
+                    Console.Write(randomRowNumber + " ");
+
+                }                
+                Console.WriteLine();
             }
         }
     }
