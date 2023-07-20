1ca72a72d7064bc5f7c8c6b698fd3e3826690d4c
tscripca <scripcatudor1@gmail.com>
2023-07-20 23:11:06 +0100

    I thought to use a flag in order to store how many times number seven appears in the middle row or any other location and then print out the winner message.

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -5,26 +5,52 @@ namespace SlotMachine // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Random rng = new Random();
-            int[,] slotMachine = new int[3, 3];
+            const int SLOT_MACHINE_ROWS = 3;
+            const int SLOT_MACHINE_COLUMNS = 3;
+            const int LUCKY_NUMBER = 7;
+            bool autoPlay = true;
+            bool checkForWinningConditions = true;
+            int storeLuckyNumbers = 0;
 
-            for (int collumnIndex = 0; collumnIndex < slotMachine.GetLength(0); collumnIndex++)
-            {  
-                if(collumnIndex > 0)
-                {
-                    int randomCollumnNumber = rng.Next(0, 10);
-                    slotMachine[collumnIndex, 0] = randomCollumnNumber;
-                }
+            Random rng = new Random();
+            int[,] slotMachine = new int[SLOT_MACHINE_ROWS, SLOT_MACHINE_COLUMNS];
+            while (autoPlay)
+            {
+                Console.Clear();
+                storeLuckyNumbers = 0;
+
                 for (int rowIndex = 0; rowIndex < slotMachine.GetLength(1); rowIndex++)
                 {
-                    int randomRowNumber = rng.Next(0, 10);
-                    slotMachine[0, rowIndex] = randomRowNumber;
-                     
-                    Console.Write(randomRowNumber + " ");
-
-                }                
-                Console.WriteLine();
-            }            
+                    for (int columnIndex = 0; columnIndex < slotMachine.GetLength(0); columnIndex++)
+                    {
+                        int numbersToFill = rng.Next(0, 10);
+                        slotMachine[rowIndex, columnIndex] = numbersToFill;
+                        Console.Write(numbersToFill + " ");
+                        if((rowIndex == 0 && rowIndex == 2) && columnIndex == 1)
+                        {
+                            if(numbersToFill == LUCKY_NUMBER)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("CROSS OF SEVENS!!!");
+                            }
+                        }
+                        if (rowIndex == 1)
+                        {
+                            if (numbersToFill == LUCKY_NUMBER)
+                            {
+                                checkForWinningConditions = true;
+                                storeLuckyNumbers++;
+                            }
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                if (storeLuckyNumbers == 3)
+                {
+                    Console.WriteLine("WINNER!");
+                    break;
+                }
+            }
         }
     }
 }
