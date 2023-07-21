251d02b9a7685cfd56055084fe9aeeba2f32cfc9
tscripca <scripcatudor1@gmail.com>
2023-07-21 17:18:11 +0100

    #4 The program finds the prints the winning combination only when the available credit is consumed.

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -8,47 +8,59 @@ namespace SlotMachine // Note: actual namespace depends on the project name.
             const int SLOT_MACHINE_ROWS = 3;
             const int SLOT_MACHINE_COLUMNS = 3;
             const int LUCKY_NUMBER = 7;
+            const int MINIMUM_PLAYABLE_AMOUNT = 1;
             bool autoPlay = true;
-            bool checkForWinningConditions = true;
             int storeLuckyNumbers = 0;
+            int creditCash = 0;
 
             Random rng = new Random();
             int[,] slotMachine = new int[SLOT_MACHINE_ROWS, SLOT_MACHINE_COLUMNS];
-            while (autoPlay)
-            {
-                Console.Clear();
-                storeLuckyNumbers = 0;
 
-                for (int rowIndex = 0; rowIndex < slotMachine.GetLength(1); rowIndex++)
+            Console.WriteLine("Minimum credit is 1.");
+            Console.Write("Insert credit: ");
+            creditCash = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            for (int moneyTracker = creditCash; moneyTracker >= MINIMUM_PLAYABLE_AMOUNT; moneyTracker--)
+            {
+                while (autoPlay)
                 {
-                    for (int columnIndex = 0; columnIndex < slotMachine.GetLength(0); columnIndex++)
+                    Console.Clear();
+                    storeLuckyNumbers = 0;
+
+                    Console.WriteLine($"\t\t\tBalance {moneyTracker}");
+
+                    for (int rowIndex = 0; rowIndex < slotMachine.GetLength(1); rowIndex++)
                     {
-                        int numbersToFill = rng.Next(0, 10);
-                        slotMachine[rowIndex, columnIndex] = numbersToFill;
-                        Console.Write(numbersToFill + " ");
-                        if((rowIndex == 0 && rowIndex == 2) && columnIndex == 1)
+                        for (int columnIndex = 0; columnIndex < slotMachine.GetLength(0); columnIndex++)
                         {
-                            if(numbersToFill == LUCKY_NUMBER)
+                            int numbersToFill = rng.Next(0, 10);
+                            slotMachine[rowIndex, columnIndex] = numbersToFill;
+                            Console.Write(numbersToFill + " ");
+                            if ((rowIndex == 0 && rowIndex == 2) && columnIndex == 1)
                             {
-                                Console.WriteLine();
-                                Console.WriteLine("CROSS OF SEVENS!!!");
+                                if (numbersToFill == LUCKY_NUMBER)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("CROSS OF SEVENS!!!");
+                                }
+                            }
+                            if (rowIndex == 1)
+                            {
+                                if (numbersToFill == LUCKY_NUMBER)
+                                {
+                                    storeLuckyNumbers++;
+                                }
                             }
                         }
-                        if (rowIndex == 1)
-                        {
-                            if (numbersToFill == LUCKY_NUMBER)
-                            {
-                                checkForWinningConditions = true;
-                                storeLuckyNumbers++;
-                            }
-                        }
+                        Console.WriteLine();
                     }
-                    Console.WriteLine();
-                }
-                if (storeLuckyNumbers == 3)
-                {
-                    Console.WriteLine("WINNER!");
-                    break;
+                    if (storeLuckyNumbers == 3)
+                    {
+                        Console.WriteLine("WINNER!");
+                        break;
+                    }
                 }
             }
         }
