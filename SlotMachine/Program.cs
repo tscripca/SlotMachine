6f1fdd562de83925fa4c0b0f6f5c7da621df5621
tscripca <scripcatudor1@gmail.com>
2023-09-16 21:05:48 +0100

    Close #15 now the if block decides which path to go and the while loop is only used  for valid input.

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -27,15 +27,13 @@ namespace SlotMachine
 
             Console.WriteLine("\t\t\t=SLOT MACHINE=");
             Console.WriteLine($"This is a {SLOT_MACHINE_ROWS} by {SLOT_MACHINE_COLUMNS} slot machine.");
-            Console.WriteLine("In this game you will be asked to insert an mount of credit of your choice.");
-            Console.WriteLine("After this you will have to choose between three game types, Horizontal, Vertical or Diagonals.");
-            Console.WriteLine($"On each game type you can bet on {BET_ONE_LINE}, {BET_TWO_LINES} up to {SLOT_MACHINE_ROWS} lines.");
-            Console.WriteLine("Credit will be deducted from your account, proportionally with number of lines you've bet on, and " +
-                $"will be added back into your account in case of a win, at the same ratio.(e.g. bet {SLOT_MACHINE_ROWS} lines and match only {BET_TWO_LINES} lines then only " +
-                $"${BET_TWO_LINES} will be added to your account.)");
+            Console.WriteLine("Insert credit amount then choose between three game types, Horizontal, Vertical or Diagonal.");            
+            Console.WriteLine($"On each game you can bet on {BET_ONE_LINE}, {BET_TWO_LINES} up to {SLOT_MACHINE_ROWS} lines or columns.");
+            Console.WriteLine("Credit will be deducted from your balance proportionally with the number of lines you're playing, and " +
+                $"will be added back into your account in case of a win for each matching line.(bet {BET_TWO_LINES} lines and match then ${BET_TWO_LINES} will be added to your account.)");
             Console.WriteLine("Press any key to start!");
             Console.ReadKey();
-            Console.Clear();   
+            Console.Clear();
 
             while (autoPlay)
             {
@@ -52,48 +50,54 @@ namespace SlotMachine
                     Console.WriteLine("1 - horizontal");
                     Console.WriteLine("2 - vertical");
                     Console.WriteLine("3 - diagonals");
-                    int userChooseGame = Convert.ToInt32(Console.ReadLine());
+                    int userChooseToPlay = Convert.ToInt32(Console.ReadLine());
                     Console.Clear();
 
-                    while (userChooseGame < 0 || userChooseGame > slotMachine.GetLength(0))
+                    while (userChooseToPlay <= 0 || userChooseToPlay > slotMachine.GetLength(0))
                     {
                         Console.WriteLine("Selection not available! Try again.");
                         Console.WriteLine("1 - horizontal");
                         Console.WriteLine("2 - vertical");
                         Console.WriteLine("3 - diagonals");
-                        userChooseGame = Convert.ToInt32(Console.ReadLine());
+                        userChooseToPlay = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine();
                         Console.Clear();
                     }
 
-                    while (userChooseGame == PLAY_HORIZONTAL || userChooseGame == PLAY_VERTICAL)
+                    if (userChooseToPlay >= PLAY_HORIZONTAL && userChooseToPlay <= PLAY_VERTICAL) //case horizontal or vertical
                     {
-                        Console.Write("Ho many lines would you like to play?(1 to 3): ");
-                        betAmount = Convert.ToInt32(Console.ReadLine());
-                        Console.Clear();
+                        while (true) //breaks when betamount is valid
+                        {
+                            Console.Write($"Ho many lines would you like to play?({BET_ONE_LINE} to {SLOT_MACHINE_ROWS}): ");
+                            betAmount = Convert.ToInt32(Console.ReadLine());
+                            Console.Clear();
 
-                        if (betAmount >= BET_ONE_LINE && betAmount <= slotMachine.GetLength(0))
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Selection not avaialable!Try again.");
+                            if (betAmount >= BET_ONE_LINE && betAmount <= slotMachine.GetLength(0))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Selection not avaialable!Try again.");
+                            }
                         }
                     }
-                    while (userChooseGame == PLAY_DIAGONALS)
-                    {
-                        Console.Write("How many diagonals would you like to play?(1 or 2): ");
-                        betAmount = Convert.ToInt32(Console.ReadLine());
-                        Console.Clear();
 
-                        if (betAmount >= BET_ONE_LINE && betAmount <= BET_TWO_LINES)
+                    if (userChooseToPlay >= PLAY_DIAGONALS)
+                    {
+                        while (true)
                         {
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Selection not avaialable!Try again.");
+                            Console.Write($"How many diagonals would you like to play?({BET_ONE_LINE} or {BET_TWO_LINES}): ");
+                            betAmount = Convert.ToInt32(Console.ReadLine());
+                            Console.Clear();
+                            if (betAmount >= BET_ONE_LINE && betAmount <= BET_TWO_LINES)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Selection not avaialable!Try again.");
+                            }
                         }
                     }
 
@@ -102,7 +106,7 @@ namespace SlotMachine
                         break;
                     }
 
-                    totalCreditBalance -= betAmount;
+                    totalCreditBalance = credits - betAmount;
                     Console.WriteLine($"\t\t\tCredit balance: ${totalCreditBalance}");
 
                     int rndNum = 0;
@@ -120,7 +124,7 @@ namespace SlotMachine
 
                     int winningRowCount = 0;
 
-                    if (userChooseGame == PLAY_HORIZONTAL)
+                    if (userChooseToPlay == PLAY_HORIZONTAL)
                     {
                         Console.WriteLine("\t\t\tPlaying horizontal!");
                         for (int rowIndex = 0; rowIndex < betAmount; rowIndex++)
@@ -141,7 +145,7 @@ namespace SlotMachine
                         }
                     }
 
-                    if (userChooseGame == PLAY_VERTICAL)
+                    if (userChooseToPlay == PLAY_VERTICAL)
                     {
                         Console.WriteLine("\t\t\tPlaying vertical!");
                         for (int columnIndex = 0; columnIndex < betAmount; columnIndex++)
@@ -162,9 +166,9 @@ namespace SlotMachine
                         }
                     }
 
-                    if (userChooseGame == PLAY_DIAGONALS)
+                    if (userChooseToPlay == PLAY_DIAGONALS)
                     {
-                        Console.WriteLine("Playing diagonals!");
+                        Console.WriteLine("\t\t\tPlaying diagonals!");
                         int matchingNumbers = 0;
                         //checking the 1st diagonal
                         for (int rowIndex = 0; rowIndex < slotMachine.GetLength(0); rowIndex++)
@@ -217,6 +221,7 @@ namespace SlotMachine
                 }
                 Console.WriteLine("Not enough credit available! Press any key to top-up!");
                 Console.ReadKey();
+                Console.Clear();
             }
         }
     }
